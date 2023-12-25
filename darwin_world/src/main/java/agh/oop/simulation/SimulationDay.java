a25206a35c304581995b086372d491412466aebe
baramundi666 <mkrol@student.agh.edu.pl>
2023-12-25 01:45:49 +0100

    created AbstractSimulationPart class which is being currently exteded by SimulationDay and SimulationInitializer

--- darwin_world/src/main/java/agh/oop/simulation/SimulationDay.java
+++ darwin_world/src/main/java/agh/oop/simulation/SimulationDay.java
@@ -3,37 +3,22 @@ package agh.oop.simulation;
 import agh.oop.model.map.Earth;
 import agh.oop.model.map.Vector2d;
 import agh.oop.model.objects.Animal;
-import agh.oop.model.objects.Plant;
-import agh.oop.model.objects.inheritance.Genome;
 import agh.oop.model.objects.inheritance.Mutation;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class SimulationDay{
-    private final Earth earth;
-    private final HashSet<Animal> animals;//statistics purpose
-    private final HashSet<Vector2d> notGrownFields;
-    private final int reproduceEnergy;
+public class SimulationDay extends AbstractSimulationPart{
+
     private final Mutation mutation;
-    private final int newPlantNumber;
-    private final int plantEnergy;
-    public int notGrownEquatorFields;
-    public final int[] equatorBorders;
+
+
 
     public SimulationDay(Earth earth, HashSet<Animal> animals, HashSet<Vector2d> notGrownFields,
-                         int newPlantNumber, int plantEnergy, int reproduceEnergy, Mutation mutation){
-        this.earth = earth;
-        this.animals = animals;
-        this.notGrownFields = notGrownFields;
-        this.reproduceEnergy = reproduceEnergy;
+                         int newPlantNumber, int plantEnergy, int reproduceEnergy,
+                         Mutation mutation) {
+        super(earth, animals, notGrownFields, newPlantNumber, plantEnergy, reproduceEnergy);
         this.mutation = mutation;
-        this.newPlantNumber = newPlantNumber;
-        this.plantEnergy = plantEnergy;
-        int lowerEquatorBorder = (int)(Math.ceil(earth.getBounds().upperRight().getY()/5.0 * 2));
-        int upperEquatorBorder = lowerEquatorBorder + (int)(Math.ceil((earth.getBounds().upperRight().getY()+1)/5.0)-1);
-        this.equatorBorders = new int[]{lowerEquatorBorder, upperEquatorBorder};
-        this.notGrownEquatorFields = (equatorBorders[1]-equatorBorders[0]+1) * (earth.getBounds().upperRight().getX()+1) - this.newPlantNumber;
     }
 
     public void simulateOneDay(){
@@ -113,33 +98,6 @@ public class SimulationDay{
         }
         for (Animal animal : toPlace){
             earth.placeAnimal(animal);
-        }
-    }
-    void spawnPlants(){
-        List<Vector2d> notGrownEquatorList = notGrownFields.stream()
-                .filter(position -> position.getY() >= equatorBorders[0] && position.getY() <= equatorBorders[1])
-                .collect(Collectors.toList());
-        List<Vector2d> notGrownSteppeList = notGrownFields.stream()
-                .filter(position -> position.getY() < equatorBorders[0] || position.getY() > equatorBorders[1])
-                .collect(Collectors.toList());
-        Collections.shuffle(notGrownEquatorList);
-        Collections.shuffle(notGrownSteppeList);
-
-        Iterator<Vector2d> equatorIterator = notGrownEquatorList.iterator();
-        Iterator<Vector2d> steppeIterator = notGrownSteppeList.iterator();
-        for(int i=0; i<newPlantNumber && i<notGrownFields.size(); i++){
-            Vector2d position;
-            int random = (int)(Math.random()*5);
-            if((random<4 || !steppeIterator.hasNext()) && equatorIterator.hasNext() ){
-                position = equatorIterator.next();
-                notGrownEquatorFields--;
-            }
-            else {
-                position = steppeIterator.next();
-            }
-            Plant newPlant = new Plant(position ,plantEnergy,false);
-            notGrownFields.remove(position);
-            earth.placePlant(newPlant);
         }
     }
 
--- darwin_world/src/main/java/agh/oop/simulation/SimulationInitializer.java
+++ darwin_world/src/main/java/agh/oop/simulation/SimulationInitializer.java
@@ -3,44 +3,24 @@ package agh.oop.simulation;
 import agh.oop.model.map.Earth;
 import agh.oop.model.map.Vector2d;
 import agh.oop.model.objects.Animal;
-import agh.oop.model.objects.Plant;
 import agh.oop.model.objects.inheritance.Genome;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class SimulationInitializer {
+public class SimulationInitializer extends AbstractSimulationPart{
 
-    private final Earth earth;
-    private final HashSet<Animal> animals;//statistics purpose
-    private final HashSet<Vector2d> notGrownFields;
-    private final int reproduceEnergy;
-    private final int newPlantNumber;
-    private final int plantEnergy;
-
-    private final int animalNumber;
     private final int genomeLength;
+    private final int animalNumber;
     private final int initialEnergy;
-    public int notGrownEquatorFields;
-    public final int[] equatorBorders;
 
-    public SimulationInitializer(Earth earth, HashSet<Animal> animals,
-                                 int newPlantNumber, int plantEnergy, int reproduceEnergy,
-                                 int animalNumber, int genomeLength, int initialEnergy){
-        this.earth = earth;
-        this.notGrownFields = new HashSet<>();
-        this.newPlantNumber = newPlantNumber;
-        this.plantEnergy = plantEnergy;
-        this.reproduceEnergy = reproduceEnergy;
-        this.animalNumber = animalNumber;
+    public SimulationInitializer(Earth earth, HashSet<Animal> animals, int newPlantNumber, int plantEnergy,
+                                 int genomeLength, int animalNumber, int initialEnergy, int reproduceEnergy) {
+        super(earth, animals, new HashSet<>(), newPlantNumber, plantEnergy, reproduceEnergy);
         this.genomeLength = genomeLength;
+        this.animalNumber = animalNumber;
         this.initialEnergy = initialEnergy;
-        this.animals = animals;
-        int lowerEquatorBorder = (int)(Math.ceil(earth.getBounds().upperRight().getY()/5.0 * 2));
-        int upperEquatorBorder = lowerEquatorBorder + (int)(Math.ceil((earth.getBounds().upperRight().getY()+1)/5.0)-1);
-        this.equatorBorders = new int[]{lowerEquatorBorder, upperEquatorBorder};
-        this.notGrownEquatorFields = (equatorBorders[1]-equatorBorders[0]+1) * (earth.getBounds().upperRight().getX()+1) - this.newPlantNumber;
     }
+
 
     public HashSet<Vector2d> getNotGrownFields() {
         return new HashSet<>(notGrownFields);
@@ -76,34 +56,6 @@ public class SimulationInitializer {
             var animal = new Animal(freePositions.get(i), initialEnergy, generateGenome(), reproduceEnergy);
             animals.add(animal);
             earth.placeAnimal(animal);
-        }
-    }
-
-    void spawnPlants(){
-        List<Vector2d> notGrownEquatorList = notGrownFields.stream()
-                .filter(position -> position.getY() >= equatorBorders[0] && position.getY() <= equatorBorders[1])
-                .collect(Collectors.toList());
-        List<Vector2d> notGrownSteppeList = notGrownFields.stream()
-                .filter(position -> position.getY() < equatorBorders[0] || position.getY() > equatorBorders[1])
-                .collect(Collectors.toList());
-        Collections.shuffle(notGrownEquatorList);
-        Collections.shuffle(notGrownSteppeList);
-
-        Iterator<Vector2d> equatorIterator = notGrownEquatorList.iterator();
-        Iterator<Vector2d> steppeIterator = notGrownSteppeList.iterator();
-        for(int i=0; i<newPlantNumber && i<notGrownFields.size(); i++){
-            Vector2d position;
-            int random = (int)(Math.random()*5);
-            if((random<4 || !steppeIterator.hasNext()) && equatorIterator.hasNext() ){
-                position = equatorIterator.next();
-                notGrownEquatorFields--;
-            }
-            else {
-                position = steppeIterator.next();
-            }
-            Plant newPlant = new Plant(position ,plantEnergy,false);
-            notGrownFields.remove(position);
-            earth.placePlant(newPlant);
         }
     }
 }
