1e6669f92d128faf5cafac740d32ea6b61c294cd
maciejmakowski2003 <maciek2608@onet.pl>
2024-01-18 09:54:09 +0100

    fix small mistakes

--- darwin_world/src/main/java/agh/oop/simulation/statictics/AnimalStatistics.java
+++ darwin_world/src/main/java/agh/oop/simulation/statictics/AnimalStatistics.java
@@ -8,7 +8,7 @@ public class AnimalStatistics {
 
     public AnimalStatistics(Animal animal, PlantEatenCountStatistics plantEatenCountStatistics,
                             DescendantsStatistics descendantsStatistics){
-        statisticsData = new StatisticsData(animal.getGenome().getGeneList(), animal.getGenome().getActiveGene(),
+        statisticsData = new StatisticsData(animal.getGenome().getGeneList(), animal.getGenome().getActiveGeneValue(),
                 animal.getEnergy(), plantEatenCountStatistics.getPlantEatenCount(animal), animal.getChildrenCount(),
                 descendantsStatistics.getNumberOfDescendants(animal), animal.getLifeLength(),
                 animal.getDayOfDeath().map(Object::toString).orElse("Alive"));
